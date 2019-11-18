187d057d40333016dd935de57d861e6a2de2f759
Davi Rodrigues Coelho <developer.davi@gmail.com>
2019-11-18 17:01:29 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const { exec } = require('child_process');
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+var state = { link: 'https://dashboard.lognature.com.br' }
+
+exec(`echo ${state.link} > /home/pi/display_link.txt`)
+exec('sudo systemctl restart kiosk')
+
+app.get('/status', (req, res) => {
+  return res.json(state)
+})
+
+app.post('/link', (req, res) => {
+  const { link } = req.body
+
+  if (!link)
+    return res.sendStatus(400)
+
+  exec(`echo ${link} > /home/pi/display_link.txt`)
+  exec('sudo systemctl restart kiosk')
+
+  return res.sendStatus(200)
+})
+
+const port = 5000
+
+http.listen(port, function() {
+	console.log('[SERVER] Successfully started! Running on port: ' + port)
+})
